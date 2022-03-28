ecaf5bd24522cc3645db9a975a65df16d6e7546f
jbreffle <78390184+jbreffle@users.noreply.github.com>
2022-03-28 11:06:56 -0400

    Create plot_randnet_results.m
    Create a function to plot results from one simulation trial

---
+++ functions/plot_randnet_results.m
@@ -0,0 +1,79 @@
+function plot_randnet_results(parameters, network, V_m, G_in, network_spike_sequences, ithTest)
+
+%Find spike profile
+spikes_V_m = V_m >= parameters.V_th;
+[spikes_x,spikes_t] = find(spikes_V_m);
+max_time = max(spikes_t);
+spiking_neurons = unique(spikes_x, 'stable');
+
+if isfield(network_spike_sequences(ithTest), 'events')
+    events = network_spike_sequences(ithTest).events;
+
+    %Visualize re-ordered spike sequences
+    if any(strcmp('spike_order',fieldnames(network_spike_sequences)))
+        f = figure;
+        axes = [];
+        num_events = size(events, 1)
+        for e_i = 1:num_events
+
+            spike_ranks = network_spike_sequences(ithTest).spike_ranks.(strcat('sequence_',string(e_i)));
+            if parameters.E_events_only
+                [~, Ie] = sort(spike_ranks);
+                eventSpikes = [spikes_V_m(network.E_indices(Ie),events(e_i,1):events(e_i,2)); ...
+                    spikes_V_m(network.I_indices,events(e_i,1):events(e_i,2))];
+            else
+                [~, Ie] = sort(spike_ranks(network.E_indices));
+                [~, Ii] = sort(spike_ranks(network.I_indices));
+                eventSpikes = [spikes_V_m(network.E_indices(Ie),events(e_i,1):events(e_i,2)); ...
+                    spikes_V_m(network.I_indices(Ii),events(e_i,1):events(e_i,2))];
+            end
+            subplot(1,num_events,e_i)
+            plotSpikeRaster( eventSpikes, 'TimePerBin', parameters.dt, 'PlotType', 'scatter');
+            xlabel('Time (s)','FontSize',16)
+            ylabel('Reordered Neuron Number','FontSize',16)
+            title(strcat('Event #',string(e_i)))
+
+        end
+        sgtitle('Spiking Behavior','FontSize',16)
+
+        %linkaxes(axes)
+        if parameters.saveFlag
+            savefig(f,strcat(net_save_path,'/','_',string(ithTest),'firing_sequence.fig'))
+            saveas(f,strcat(net_save_path,'/', '_',string(ithTest),'firing_sequence.jpg'))
+            close(f)
+        end
+        clear e_i spike_order reordered_spikes event_length s_i ax axes xt xtlbl
+    end      
+end
+
+t = [0:parameters.dt:parameters.t_max];
+figure; plot(t, V_m(1:2,:)); ylabel('Vm (V)'); xlabel('Time (s)'); 
+% figure; plot(t, V_m); ylabel('Vm (V)'); xlabel('Time (s)'); 
+figure; plot(t, G_in(1:2,:)); ylabel('G in (S)'); xlabel('Time (s)'); 
+% figure; plot(t, G_in(1:2,:)); ylabel('G in (S)'); xlabel('Time (s)'); 
+
+figure; hold on
+if exist('events')==1 
+    for i = 1:size(events, 1)
+        fill([t(events(i,:)), fliplr(t(events(i,:)))], [0, 0, size(spikes_V_m, 1), size(spikes_V_m, 1)], 'k', 'FaceAlpha', 0.2, 'EdgeColor', 'none')
+    end
+end
+plotSpikeRaster( [spikes_V_m(network.E_indices,:);  spikes_V_m(network.I_indices,:)], 'TimePerBin', parameters.dt, 'PlotType', 'scatter'); 
+ylabel('Cell'); xlabel('Time (s)'); 
+
+
+movmeanWindow = (1/parameters.dt) * 0.05;
+meanPopRate = movmean(mean(spikes_V_m, 1)/parameters.dt, movmeanWindow);
+figure; hold on
+if exist('events')==1 
+    for i = 1:size(events, 1)
+        fill([t(events(i,:)), fliplr(t(events(i,:)))], [0, 0, ceil(max(meanPopRate)), ceil(max(meanPopRate))], 'k', 'FaceAlpha', 0.2, 'EdgeColor', 'none')
+    end
+end
+plot(t, meanPopRate)
+ylabel('Population mean rate (Hz)'); xlabel('Time (s)'); 
+yline(mean(meanPopRate), 'g')
+yline(mean(meanPopRate)+ std(meanPopRate))
+yline(mean(meanPopRate)+ 2*std(meanPopRate))
+
+end
--- randnet.m
+++ randnet.m
@@ -5,11 +5,11 @@
 
 clear all
 
-saveFlag = 0; % 1 to save simulation results
-selectPath = 0; % 1 to select save destination, 0 to save in current dir
-plotResults = 1; % 1 to plot basic simulation results
+parameters.saveFlag = 0; % 1 to save simulation results
+parameters.selectPath = 0; % 1 to select save destination, 0 to save in current dir
+parameters.plotResults = 1; % 1 to plot basic simulation results
 
-if selectPath
+if parameters.selectPath
     save_path = uigetdir('/Users/hannahgermaine/Documents/PhD/','Select Save Folder'); %Have user input where they'd like the output stored
 else
     save_path = [pwd, '/results'];
@@ -71,7 +71,8 @@ parameters.global_inhib = 1; % if 1, I-cells are not clustered and have connecti
 parameters.p_I = 0.5; % probability of an I cell connecting to any other cell
 
 % Number of trials per net to run
-parameters.test_val_max = 1; % How many tests of different initializations to run
+parameters.nTrials = 1; % How many tests of different initializations to run
+parameters.nNets = 1; % How many networks to run
 
 
 %% Parameters for sequence analysis
@@ -116,11 +117,11 @@ parameters.n_I = round((1-parameters.p_E)*parameters.n); %number of inhibitory n
 %If uploading a parameter file, uncomment the next line
 % load(strcat(save_path,'/parameters.mat'))
 
-if saveFlag & ~isfolder(save_path)
+if parameters.saveFlag & ~isfolder(save_path)
     mkdir(save_path);
 end
     
-if saveFlag
+if parameters.saveFlag
     save(strcat(save_path,'/parameters.mat'),'parameters'); 
 end
 
@@ -128,17 +129,17 @@ end
 %___Run Simulations for Different Networks___
 %____________________________________________
 
-for i = 1:1%10 %how many different network structures to test
+for ithNet = 1:parameters.nNets
     
     %CREATE NETWORK SAVE PATH
-    net_save_path = strcat(save_path,'/network_',string(i));
-    if saveFlag & ~isfolder(net_save_path)
+    net_save_path = strcat(save_path,'/network_',string(ithNet));
+    if parameters.saveFlag & ~isfolder(net_save_path)
         mkdir(net_save_path);
     end
     
     % Generete ith network structure
-    network = create_clusters(parameters, 'seed', i, 'include_all', parameters.include_all, 'global_inhib', parameters.global_inhib);
-    if saveFlag
+    network = create_clusters(parameters, 'seed', ithNet, 'include_all', parameters.include_all, 'global_inhib', parameters.global_inhib);
+    if parameters.saveFlag
         save(strcat(net_save_path,'/network.mat'),'network');
     end
     
@@ -151,7 +152,7 @@ for i = 1:1%10 %how many different network structures to test
     network_spike_sequences = struct;
     network_cluster_sequences = struct; 
     
-    for ithTest = 1:parameters.test_val_max        
+    for ithTest = 1:parameters.nTrials       
         
         %Create input conductance variable
         if parameters.usePoisson
@@ -186,90 +187,15 @@ for i = 1:1%10 %how many different network structures to test
         [network_spike_sequences, network_cluster_sequences] = detect_events(parameters, network, V_m , ithTest,...
             network_spike_sequences, network_cluster_sequences, 'E_events_only', parameters.E_events_only);
     
-        if plotResults
-
-            %Find spike profile
-            spikes_V_m = V_m >= parameters.V_th;
-            [spikes_x,spikes_t] = find(spikes_V_m);
-            max_time = max(spikes_t);
-            spiking_neurons = unique(spikes_x, 'stable');
-
-            if isfield(network_spike_sequences(ithTest), 'events')
-                events = network_spike_sequences(ithTest).events;
-
-                %Visualize re-ordered spike sequences
-                if any(strcmp('spike_order',fieldnames(network_spike_sequences)))
-                    f = figure;
-                    axes = [];
-                    num_events = size(events, 1)
-                    for e_i = 1:num_events
-                        
-                        spike_ranks = network_spike_sequences(ithTest).spike_ranks.(strcat('sequence_',string(e_i)));
-                        if parameters.E_events_only
-                            [~, Ie] = sort(spike_ranks);
-                            eventSpikes = [spikes_V_m(network.E_indices(Ie),events(e_i,1):events(e_i,2)); ...
-                                spikes_V_m(network.I_indices,events(e_i,1):events(e_i,2))];
-                        else
-                            [~, Ie] = sort(spike_ranks(network.E_indices));
-                            [~, Ii] = sort(spike_ranks(network.I_indices));
-                            eventSpikes = [spikes_V_m(network.E_indices(Ie),events(e_i,1):events(e_i,2)); ...
-                                spikes_V_m(network.I_indices(Ii),events(e_i,1):events(e_i,2))];
-                        end
-                        subplot(1,num_events,e_i)
-                        plotSpikeRaster( eventSpikes, 'TimePerBin', parameters.dt, 'PlotType', 'scatter');
-                        xlabel('Time (s)','FontSize',16)
-                        ylabel('Reordered Neuron Number','FontSize',16)
-                        title(strcat('Event #',string(e_i)))
-                        
-                    end
-                    sgtitle('Spiking Behavior','FontSize',16)
-
-                    %linkaxes(axes)
-                    if saveFlag
-                        savefig(f,strcat(net_save_path,'/','_',string(ithTest),'firing_sequence.fig'))
-                        saveas(f,strcat(net_save_path,'/', '_',string(ithTest),'firing_sequence.jpg'))
-                        close(f)
-                    end
-                    clear e_i spike_order reordered_spikes event_length s_i ax axes xt xtlbl
-                end      
-            end
-            
-            t = [0:parameters.dt:parameters.t_max];
-            figure; plot(t, V_m(1:2,:)); ylabel('Vm (V)'); xlabel('Time (s)'); 
-            % figure; plot(t, V_m); ylabel('Vm (V)'); xlabel('Time (s)'); 
-            figure; plot(t, G_in(1:2,:)); ylabel('G in (S)'); xlabel('Time (s)'); 
-            % figure; plot(t, G_in(1:2,:)); ylabel('G in (S)'); xlabel('Time (s)'); 
-
-            figure; hold on
-            if exist('events')==1 
-                for i = 1:size(events, 1)
-                    fill([t(events(i,:)), fliplr(t(events(i,:)))], [0, 0, size(spikes_V_m, 1), size(spikes_V_m, 1)], 'k', 'FaceAlpha', 0.2, 'EdgeColor', 'none')
-                end
-            end
-            plotSpikeRaster( [spikes_V_m(network.E_indices,:);  spikes_V_m(network.I_indices,:)], 'TimePerBin', parameters.dt, 'PlotType', 'scatter'); 
-            ylabel('Cell'); xlabel('Time (s)'); 
-            
-
-            movmeanWindow = (1/parameters.dt) * 0.05;
-            meanPopRate = movmean(mean(spikes_V_m, 1)/parameters.dt, movmeanWindow);
-            figure; hold on
-            if exist('events')==1 
-                for i = 1:size(events, 1)
-                    fill([t(events(i,:)), fliplr(t(events(i,:)))], [0, 0, ceil(max(meanPopRate)), ceil(max(meanPopRate))], 'k', 'FaceAlpha', 0.2, 'EdgeColor', 'none')
-                end
-            end
-            plot(t, meanPopRate)
-            ylabel('Population mean rate (Hz)'); xlabel('Time (s)'); 
-            yline(mean(meanPopRate), 'g')
-            yline(mean(meanPopRate)+ std(meanPopRate))
-            yline(mean(meanPopRate)+ 2*std(meanPopRate))
+        if parameters.plotResults
+            plot_randnet_results(parameters, network, V_m, G_in, network_spike_sequences, ithTest)
         end
         
-    end
+    end % trial loop
     
     
     %SAVE NETWORK DATA
-    if saveFlag
+    if parameters.saveFlag
         save(strcat(net_save_path,'/V_m_var.mat'),'V_m_var','-v7.3')
         save(strcat(net_save_path,'/G_var.mat'),'G_var','-v7.3')
         save(strcat(net_save_path,'/I_var.mat'),'I_var','-v7.3')
@@ -277,13 +203,13 @@ for i = 1:1%10 %how many different network structures to test
         save(strcat(net_save_path,'/network_cluster_sequences.mat'),'network_cluster_sequences','-v7.3')
     end
     
-end %End of network structure loop
+end % Network loop
+
 
 try % Print some simple simulation results
-    disp(events); 
+    disp(network_spike_sequences(end).events); 
     structfun(@sum, network_spike_sequences.nonspiking_neurons)
 catch
     disp('No events detected');
 end
-
 

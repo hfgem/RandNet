6cf66ee56f946fe7863bcb707fa065204a570557
jbreffle <78390184+jbreffle@users.noreply.github.com>
2022-05-23 12:39:06 -0400

    Create PF parallel code
    Copy parallelize_parameter_tests_2.m into parallelize_parameter_tests_2_PF.m
    
    Copy randnet_param_tests.m into randnet_PF_param_tests.m

---
+++ functions/parallelize_parameter_tests_2_PF.m
@@ -0,0 +1,168 @@
+function [avg_mat, allResults] = parallelize_parameter_tests_2(parameters,num_nets,...
+    num_inits, parameterSets_vec, ithParamSet, variedParam)
+    %_________
+    %ABOUT: This function runs through a series of commands to test the
+    %outputs of a particular parameter set in comparison to a strict set of
+    %criteria. This function is to be used in conjunction with
+    %network_tests2.m where a parallelized for loop calls this function,
+    %parallelize_networks.m where network initializations are parallelized,
+    %and parallelize_network_tests.m where network tests are parallelized.
+    %
+    %INPUTS:
+    %   parameters = a structure that contains the following (only
+    %   relevant listed below):
+    %       n = Number of neurons in the network
+    %       clusters = Number of clusters of neurons in network
+    %       t_max = maximum time of simulation (s)
+    %       dt = timestep of simulation (s)
+    %       tau_syn_E = AMPA synaptic decay time constant (s) [Ignoring NMDA as slow and weak]
+    %       tau_syn_I = GABA synaptic decay time constant (s)
+    %       tau_stdp = STDP decay time constant (s)
+    %       E_K = Potassium reversal potential (V)
+    %       E_L = Leak reversal potential (V)
+    %       G_L = Leak conductance (S) - 10-30 nS range
+    %       C_m = Total membrane capacitance (F)
+    %       V_m_noise = Magnitude of membrane potential simulation noise (V)
+    %       V_th = The threshold membrane potential (V)
+    %       V_reset = The reset membrane potential (V)
+    %       V_syn_E = Excitatory synaptic reversal potential (V)
+    %       V_syn_I = Inhibitory synaptic reversal potential (V)
+    %       del_G_syn_E_E = Synaptic conductance step for 
+    %               excitatory-excitatory neuron connections following
+    %               spike (S)
+    %       del_G_syn_E_I = Synaptic conductance step for 
+    %               excitatory-inhibitory neuron connections following
+    %               spike (S)
+    %       del_G_syn_I_I = Synaptic conductance step for 
+    %               inhibitory-inhibitory neuron connections following
+    %               spike (S)
+    %       del_G_syn_I_E = Synaptic conductance step for 
+    %               inhibitory-excitatory neuron connections following
+    %               spike (S)
+    %       del_G_sra = spike rate adaptation conductance step following spike 
+    %               ranges from 1-200 *10^(-9) (S)
+    %       tau_sra = Spike rate adaptation time constant (s)
+    %       connectivity_gain = Amount to increase or decrease connectivity by 
+    %               with each spike (more at the range of 1.002-1.005) -
+    %               keep at 1 to ensure no connectivity change
+    %       G_coeff = input conductance coefficient (setting strength) (S)
+    %       G_scale = input conductance scale (ex. nano = 1*10^(-9)) (S)
+    %       t_steps = The number of timesteps in the simulation
+    %       syn_E = An [n x 1] vector of the synaptic reversal potential for
+    %               excitatory connections (V)
+    %       syn_I = An [n x 1] vector of the synaptic reversal potential for
+    %               inhibitory connections (V)     
+    %   num_nets = number of network structures to test per parameter set 
+    %   num_inits = number of network initializations to test per network
+    %       structure
+    %   parameter_vec = a matrix of size [num_params,test_n] that
+    %       contains parameter values to test
+    %   test_n = number of parameter values to test
+    %   ind = what index of parameter combinations is being tested
+    %   save_path = where to save results
+    %OUTPUTS:
+    %   avg_mat = A vector representing average values from all network
+    %   initializations and the firing initializations for each network
+    %   structure. Namely, the first dimension length is the number of
+    %   networks, the second dimension length is the number of
+    %   initializations, and the third dimension length is the 3 parameters
+    %   from each combination:
+    %       1. number of spiking neurons
+    %       2. average firing rate
+    %       3. average event length
+    %_________
+    
+
+    % Set up parameter values for current parameter set
+    for i = 1:size(variedParam, 2)
+        parameters.(variedParam(i).name) = parameterSets_vec(i,ithParamSet);
+    end
+
+    
+    % Update any parameters that are dependent on a varied parameter
+    parameters = set_depedent_parameters(parameters);
+
+    %Run network initialization code
+    resp_mat = zeros(num_nets, 4);
+    allResults = cell(1, num_nets) ;
+    for ithNet = 1:num_nets
+        
+        network = create_clusters(parameters, 'seed', ithNet, 'include_all', parameters.include_all, 'global_inhib', parameters.global_inhib);
+        
+        mat = zeros(num_inits,4);
+        network_spike_sequences = struct; 
+        for ithTest = 1:num_inits
+            seed = ithTest;
+
+            %Create input conductance variable
+            if parameters.usePoisson
+                G_in = single(zeros(parameters.n, parameters.t_steps+1));
+                for k = 2:(parameters.t_steps+1)
+                    G_in(:,k) = G_in(:,k-1)*exp(-parameters.dt/parameters.tau_syn_E);
+                    G_in(:,k) = G_in(:,k) + parameters.W_gin * [rand(parameters.n, 1) < (parameters.dt*parameters.rG)];
+                end
+            else
+                G_in = (parameters.G_std*randn(parameters.n,parameters.t_steps+1))+parameters.G_mean;
+                G_in(G_in<0) = 0;
+            end
+            parameters.('G_in') = G_in;
+            clear G_in
+            
+            %Run model
+            
+            %Create Storage Variables
+            % V_m = zeros(parameters.n,parameters.t_steps+1); %membrane potential for each neuron at each timestep
+            % V_m(:,1) = parameters.V_reset + randn([parameters.n,1])*parameters.V_m_noise; %set all neurons to baseline reset membrane potential with added noise
+            % [V_m, ~, ~, ~, ~] = randnet_calculator(parameters, seed, network, V_m);
+            % E_spikes_V_m = sparse(V_m(network.E_indices,:) >= parameters.V_th);
+            V_m = parameters.V_reset + randn([parameters.n,1])*parameters.V_m_noise; %set all neurons to baseline reset membrane potential with added noise
+            spikeMat = randnet_calculator_memOpt(parameters, seed, network, V_m);
+            parameters = rmfield(parameters, 'G_in');
+            E_spikes_V_m = spikeMat(network.E_indices,:); clear spikeMat
+            
+
+            % detect events and compute outputs
+            % [network_spike_sequences, network_cluster_sequences, outputVec] = detect_events(parameters, network, V_m , j, network_spike_sequences, network_cluster_sequences);
+
+            [trialResults] = detect_PBE(E_spikes_V_m, parameters);
+            if ithTest == 1 % append trialResults struct to network results struct
+                network_spike_sequences = trialResults;
+            else
+                network_spike_sequences = [network_spike_sequences, trialResults]; 
+            end
+            
+            % Overall simulation statistics
+            allResults{ithNet}{ithTest}.ithInit = ithTest;
+            allResults{ithNet}{ithTest}.numEvents = numel(network_spike_sequences(ithTest).event_lengths); % number of detected events
+            allResults{ithNet}{ithTest}.fracFire =  mean(sum(E_spikes_V_m, 2)>0); % Fraction of cells that fire at all during simulation
+            allResults{ithNet}{ithTest}.frac_participation = mean([network_spike_sequences(ithTest).frac_spike{:}]); % mean fraction of cells firing per event
+            allResults{ithNet}{ithTest}.meanRate = mean(sum(E_spikes_V_m, 2)/parameters.t_max); % mean over cells' average firing rate
+            allResults{ithNet}{ithTest}.stdRate = std(sum(E_spikes_V_m, 2)/parameters.t_max); % STD over cells' average firing rate
+
+            % Stats for each detected event
+            allResults{ithNet}{ithTest}.eventLength = network_spike_sequences(ithTest).event_lengths; % duration in seconds of all detected events
+            allResults{ithNet}{ithTest}.eventParticipation = [network_spike_sequences(ithTest).frac_spike{:}]; % fraction of cells that fired in each event
+                        
+            % Save ranks_vec
+            allResults{ithNet}{ithTest}.ranksVec = network_spike_sequences(ithTest).ranks_vec;
+            
+            % Main output statistics
+            %outputVec(1) = allResults{ithNet}{ithTest}.fracFire; % fraction of spiking neurons over entire simulation
+            outputVec(1) = allResults{ithNet}{ithTest}.frac_participation; % fraction of spiking neurons over identified events
+            outputVec(2) = allResults{ithNet}{ithTest}.meanRate ; %average firing rate
+            outputVec(3) = mean(allResults{ithNet}{ithTest}.eventLength); % Average event length
+            outputVec(4) = allResults{ithNet}{ithTest}.numEvents; % Number of events
+
+            mat(ithTest,:) = outputVec; 
+            %allResults{ithNet}{j} = allTrialResults;
+        end % trial loop
+        mat(isnan(mat)) = 0;
+        resp_mat(ithNet,:) = sum(mat,1) ./ sum(mat > 0,1); %Only averaging those that did successfully produce data        
+        
+    end % Network loop
+    resp_mat(isnan(resp_mat)) = 0;
+    avg_mat = sum(resp_mat,1)./sum(resp_mat > 0,1); %Only averaging those that had results
+
+    disp(['Parameter set ', num2str(ithParamSet), '/', num2str(size(parameterSets_vec, 2)), ' complete'])
+
+end % Function
---
+++ randnet_PF_param_tests.m
@@ -0,0 +1,284 @@
+%Testing different network parameters for sequence generation
+
+%______ABOUT______:
+%This code uses a baseline set of network parameters by
+%loading them (lif_network_postrotation.m must have been run to generate
+%parameter files) and then grid searches the parameter space of defined
+%parameters.
+%
+%Each parameter set will be tested for num_nets network initializations and
+%num_inits randomized neuron initializations per network. The results of 
+%each initialization will be compared against criteria of a successful
+%output and a fractional score will be calculated. This score will be 
+%stored in a 4D matrix of the parameter space.
+%
+%The results of the grid search will be visualized and characterized for a
+%parameter set description of best results. Depending on the results,
+%another grid search can be performed narrowing ranges of parameters
+%manually to determine the best parameter space for solutions.
+%__________________
+%
+% New parallelized parameter grid code, based on simulation function randnet_calculator:
+% randnet_param_tests.m 
+%   ->parallelize_parameter_tests_2 
+%           ->parallelize_network_tests_2
+%               ->randnet_calculator
+
+%% Save Path + Load Parameters
+addpath('functions')
+
+saveFlag = 1 % 1 to save simulation results
+selectSavePath = 0; % 1 to select save destination, 0 to save in results dir
+selectLoadPath = 1; % 1 to select load source, 0 to load from results dir
+plotResults = 1; % 1 to plot basic simulation results
+scriptFolder = '/param_tests'; % sub-folder so save analysis results to
+
+% Save path
+if selectSavePath
+    save_path = uigetdir('/Users/hannahgermaine/Documents/PhD/'); %Have user input where they'd like the output stored
+else
+    save_path = [pwd, '/results', scriptFolder];
+end
+
+if saveFlag & ~isfolder(save_path)
+    mkdir(save_path);
+end
+
+% Load parameters
+if selectLoadPath
+    load_path = uigetdir('/Users/hannahgermaine/Documents/PhD/');
+else
+    load_path = [pwd, '/results'];
+end
+load(strcat(load_path,'/parameters.mat'))
+
+
+%% Parameters that are different from the loaded parameters
+
+% Analysis parameters
+%{
+{assert(parameters.E_events_only==1)
+parameters.event_cutoff = 0;
+parameters.min_avg_fr = 0.001;
+parameters.max_avg_fr= inf;
+parameters.min_avg_length = 0;
+parameters.max_avg_length = inf;
+%}
+
+% Simulation duration
+%parameters.t_max = 10;
+parameters.t_max = 60;
+
+% __Necessary to override the loaded parameters__ %
+parameters.saveFlag = saveFlag;
+parameters.plotResults = plotResults; 
+
+
+%% __set/update dependent parameters__ %%
+parameters = set_depedent_parameters(parameters);
+
+if saveFlag
+    save(strcat(save_path,'/parameters.mat'),'parameters')
+end
+
+
+%% Set Up Grid Search Parameters
+
+%Test parameters
+num_nets = 3;
+% num_nets = 4;
+num_inits = 1;
+test_n = 20; % Number of parameters to test (each)
+
+
+% % temp, for testing code
+%{
+num_nets = 2;
+num_inits = 1;
+test_n = 4;
+%}
+% %
+assert(parameters.usePoisson==1)
+
+
+% Parameters must be a field in the parameter structure, and cannot be a
+% dependent parameter set in set_depedent_parameters
+
+%{
+variedParam(1).name = 'W_gin'; % 1st parameter to be varied. Must be a field in the parameter structure
+variedParam(1).range = linspace(550*10^(-12), 950*10^(-12), test_n); % set of values to test param1 at
+
+variedParam(2).name = 'del_G_syn_E_E'; % 2nd parameter to be varied
+variedParam(2).range = linspace(550*10^(-12), 950*10^(-12), test_n); % set of values to test param2 at
+%}
+
+%{
+variedParam(1).name = 'mnc'; % 2nd parameter to be varied
+%variedParam(1).range = linspace(1, 21, 81); % set of values to test param2 at
+variedParam(1).range = linspace(1, 6, 21); % set of values to test param2 at
+
+variedParam(2).name = 'clusters'; % 2nd parameter to be varied
+%variedParam(2).range = [2:1:21]; % set of values to test param2 at
+variedParam(2).range = [2:2:36]; % set of values to test param2 at
+%}
+
+variedParam(1).name = 'del_G_syn_E_E'; % 2nd parameter to be varied
+variedParam(1).range = linspace(550*10^(-12), 950*10^(-12), test_n); % set of values to test param2 at
+
+variedParam(2).name = 'del_G_syn_I_E'; % 2nd parameter to be varied
+variedParam(2).range =  linspace(300*10^(-12), 700*10^(-12), test_n); % set of values to test param2 at
+
+parameters.del_G_syn_E_I = nan;
+
+% Combine into one parameter vector to pass to parfor function
+parameterSets_vec = combvec(variedParam(:).range);
+
+
+% Exclude cases where mnc>clusters
+if isequal(variedParam(1).name, 'mnc') && isequal(variedParam(2).name, 'clusters')
+    parameterSets_vec = parameterSets_vec(:,~[parameterSets_vec(1,:)>parameterSets_vec(2,:)]);
+end
+
+
+%% Run Grid Search With Spike Stats Returned
+
+gcp; % starts parallel pool if not already running
+
+% Waitbar code
+D = parallel.pool.DataQueue;
+h = waitbar(0, 'Starting simulation ...');
+num_files = size(parameterSets_vec, 2);
+nUpdateWaitbar(num_files, h); % Dummy call to nUpdateWaitbar to initialise
+afterEach(D, @nUpdateWaitbar);
+
+tic
+resultsMatLinear = zeros(4, size(parameterSets_vec, 2));
+resultsStructLinear = cell(1, size(parameterSets_vec, 2));
+parfor ithParamSet = 1:size(parameterSets_vec, 2)
+    
+    [resultsMatLinear(:,ithParamSet), resultsStructLinear{ithParamSet}] = parallelize_parameter_tests_2(...
+                parameters, num_nets, num_inits, parameterSets_vec, ithParamSet, variedParam);
+    send(D, 1);
+end
+runTime = toc
+
+%% Format results matrix
+
+resultsMat = nan([cellfun(@length, {variedParam.range}), 4]);
+resultsStruct = struct;
+for i = 1:size(resultsMatLinear, 2)
+    
+    structIndices = {};
+    for ithParam = 1:size(variedParam, 2)
+        structIndices{ithParam} = find(parameterSets_vec(ithParam,i)==variedParam(ithParam).range);
+    end
+    resultsMat(structIndices{:},:) = resultsMatLinear(:,i);
+    
+    for j = 1:num_nets
+        for k = 1:num_inits
+            resultsStruct(structIndices{:}, j, k).results = resultsStructLinear{i}{j}{k};
+        end
+    end
+end
+
+nSlices = repmat({':'},ndims(resultsMat)-1,1); % get n slices, ":", 
+
+frac_partic = resultsMat(nSlices{:},1);
+avg_fr = resultsMat(nSlices{:},2);
+avg_event_length = resultsMat(nSlices{:},3);
+avg_n_events = resultsMat(nSlices{:},4);
+
+
+if saveFlag
+    save(strcat(save_path,'/parameterSets_vec.mat'),'parameterSets_vec','-v7.3')
+    save(strcat(save_path,'/results.mat'),'resultsMat', 'resultsStruct', '-v7.3')
+   
+    
+    clear D h % Don't save pool.DataQueue or waitbar handle
+    clear resultsStructLinear resultsMatLinear % don't save redundant data
+    
+    % Save everything, with unique filename based on date-time
+    save( strcat(save_path,'/results_', datestr(now,'yyyy-mm-ddTHH-MM'), '.mat'), '-v7.3') 
+end
+
+%% Visualize Value Grid Search Results
+
+if plotResults
+    
+    % select index of parameters to plot against each other
+    % Note: below code is not generalized to arbitrary n of variedParam
+    paramPlot1 = 1;
+    paramPlot2 = 2;
+
+    % paramPlot1 v paramPlot2
+    
+
+    %{
+    frac_partic = squeeze(mean(frac_partic,3))';
+    avg_fr = squeeze(mean(avg_fr,3))';
+    avg_event_length = squeeze(mean(avg_event_length,3))';
+    avg_n_events = squeeze(mean(avg_n_events,3))';
+    %}
+    % avg_n_events = resultsMat(nSlices{:},4);
+    
+    figure;
+    subplot(2,2,1)
+    imagesc(variedParam(paramPlot1).range, variedParam(paramPlot2).range, frac_partic', 'AlphaData', ~isnan(frac_partic'))
+    set(gca,'YDir','normal')
+    c1 = colorbar(); c1.Label.String = 'Fraction of neurons';
+    title('Frac. firing (event)'); xlabel(variedParam(paramPlot1).name,'Interpreter','none'); ylabel(variedParam(paramPlot2).name,'Interpreter','none')
+    
+    subplot(2,2,2)
+    imagesc(variedParam(paramPlot1).range, variedParam(paramPlot2).range, avg_fr', 'AlphaData', ~isnan(avg_fr'))
+    set(gca,'YDir','normal')
+    c2 = colorbar(); c2.Label.String = "Hz";
+    title('Mean spike rate (trial)'); xlabel(variedParam(paramPlot1).name,'Interpreter','none'); ylabel(variedParam(paramPlot2).name,'Interpreter','none')
+    
+    subplot(2,2,3)
+    imagesc(variedParam(paramPlot1).range, variedParam(paramPlot2).range, avg_event_length', 'AlphaData', ~isnan(avg_event_length'))
+    set(gca,'YDir','normal')
+    c3 = colorbar(); c3.Label.String = "Seconds";
+    title('Mean Event Length'); xlabel(variedParam(paramPlot1).name,'Interpreter','none'); ylabel(variedParam(paramPlot2).name,'Interpreter','none')
+    clear c1 c2 c3
+    
+    subplot(2,2,4)
+    imagesc(variedParam(paramPlot1).range, variedParam(paramPlot2).range, avg_n_events'/parameters.t_max, 'AlphaData', ~isnan(avg_n_events'))
+    set(gca,'YDir','normal')
+    c3 = colorbar(); c3.Label.String = "nEvents / s";
+    title('Mean event frequency'); xlabel(variedParam(paramPlot1).name,'Interpreter','none'); ylabel(variedParam(paramPlot2).name,'Interpreter','none')
+    clear c1 c2 c3
+
+    
+end
+
+%% Plot directly from resultsStruct
+%{
+paramPlot1 = 1;
+paramPlot2 = 2;
+% X = nanmean(arrayfun(@(x)mean(x.results.stdRate), resultsStruct), 3);
+X = nanmean(arrayfun(@(x)mean(x.results.frac_participation{1}), resultsStruct), 3);
+figure; imagesc(variedParam(paramPlot1).range, variedParam(paramPlot2).range, X', 'AlphaData', ~isnan(X'))
+set(gca,'YDir','normal')
+c2 = colorbar(); c2.Label.String = "Mean Frac participation";
+title('Additional plotting'); xlabel(variedParam(paramPlot1).name,'Interpreter','none'); ylabel(variedParam(paramPlot2).name,'Interpreter','none')
+%}
+
+%% Functions 
+function p = nUpdateWaitbar(data, h)
+% https://www.mathworks.com/matlabcentral/answers/660793-help-with-parfor-progress-bar-using-data-queue
+persistent TOTAL COUNT H
+if nargin == 2
+    % initialisation mode
+    H = h;
+    TOTAL = data;
+    COUNT = 0;
+else
+    % afterEach call, increment COUNT
+    COUNT = 1 + COUNT;
+    p = COUNT / TOTAL;
+    waitbar(p, H, ...
+        ['Simulation ', num2str(COUNT), ' of ', num2str(TOTAL), ' complete', newline...
+         'Runtime: ', datestr(datenum(0,0,0,0,0,toc),'HH:MM:SS'), ...
+        ', Remaining time: ', datestr(datenum(0,0,0,0,0,toc/p-toc),'HH:MM:SS')]);
+end
+end

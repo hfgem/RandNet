9a53819ae62e702f5c5c0da5e51f1433917762bd
jbreffle <78390184+jbreffle@users.noreply.github.com>
2022-09-29 16:55:52 -0400

    plotPFs add map corr and peaks corr

--- functions/plotPFs.m
+++ functions/plotPFs.m
@@ -1,7 +1,16 @@
 function plotPFs(allLinfields, allPFpeaksSeq, network, pfsim)
+%
+%
+% Note: the correlation of the sequence of peaks may be giving falsely high
+% correlations because (since there are only 50 spatial PF bins) there were
+% many cells that had peaks in the same spatial bin, and tied cells were
+% ordered by their index, for all environments
+% Should tied cell's be randomized? (by environment? Or by bootstrapping?)
+
 
 day = 1; epoch = 1; tetrode = 1; tr = 1;
 
+minPeakRate = 0;
 
 %% Convert place fields to matrix
 
@@ -63,7 +72,7 @@ xlabel('Position (2 cm bin)'); ylabel('Cell (sorted)');
 end
 
 %% Plot place fields by env1 peaks
-
+%{
 for ithEnv = 1:pfsim.nEnvironments 
 
 PFmat = allEnvPFs{ithEnv}';
@@ -103,6 +112,8 @@ xlabel('Position (2 cm bin)'); ylabel('Cell (sorted)');
 
 
 end
+%}
+
 
 %% Plot place fields by all different peak sequences
 
@@ -110,9 +121,9 @@ figure
 for ithEnvSequence = 1:pfsim.nEnvironments 
     
     PFpeaksSequence = allPFpeaksSeq(:,ithEnvSequence);
-    if mod(ithEnvSequence,2)==0
-        PFpeaksSequence = flip(PFpeaksSequence);
-    end
+    %if mod(ithEnvSequence,2)==0
+    %    PFpeaksSequence = flip(PFpeaksSequence);
+    %end
 
     for ithEnvData = 1:pfsim.nEnvironments 
 
@@ -137,6 +148,9 @@ for ithEnvSequence = 1:pfsim.nEnvironments
         ithPlot = sub2ind( [pfsim.nEnvironments, pfsim.nEnvironments], ithEnvSequence, ithEnvData);
         subplot(pfsim.nEnvironments, pfsim.nEnvironments, ithPlot);
         imagesc( PFmat_E(PFpeaksSequence,:)./rateDenom1 ); 
+        
+        mapCorr = nanmean(diag(corr( allEnvPFs{ithEnvSequence},  allEnvPFs{ithEnvData})))
+        title(['Map corr=', num2str(mapCorr)])
         % colorbar; caxis([0, caxmax])
         %title(['Env ID ', num2str(ithEnv)]); 
         %xlabel('Position (2 cm bin)'); ylabel('Cell (sorted)');
@@ -144,5 +158,49 @@ for ithEnvSequence = 1:pfsim.nEnvironments
 
     end
 end
+
+%% Plot peak sequence correlations
     
+figure
+for ithEnv1 = 1:pfsim.nEnvironments 
+    
+    PFpeaksSequence1 = allPFpeaksSeq(:,ithEnv1);
+    if mod(ithEnv1,2)==0
+        PFpeaksSequence1 = flip(PFpeaksSequence1);
+    end
+    PFmat = allEnvPFs{ithEnv1}';
+    PFmat_E = PFmat(network.E_indices,:);
+    [peakRate, peakRateLocation_all] = max(PFmat_E, [], 2);  
+    
+    PFpeaksSequence1(peakRate<minPeakRate) = nan;
+        
+    for ithEnv2 = 1:pfsim.nEnvironments 
+
+        PFpeaksSequence2 = allPFpeaksSeq(:,ithEnv2);
+        if mod(ithEnv2,2)==0
+            PFpeaksSequence2 = flip(PFpeaksSequence2);
+        end
+        PFmat = allEnvPFs{ithEnv2}';
+        PFmat_E = PFmat(network.E_indices,:);
+        [peakRate, peakRateLocation_all] = max(PFmat_E, [], 2);  
+
+        PFpeaksSequence2(peakRate<minPeakRate) = nan;
+    
+
+        ithPlot = sub2ind( [pfsim.nEnvironments, pfsim.nEnvironments], ithEnv1, ithEnv2);
+        subplot(pfsim.nEnvironments, pfsim.nEnvironments, ithPlot);
+        scatter(PFpeaksSequence1, PFpeaksSequence2 ); 
+        
+        [RHO,PVAL] = corr(PFpeaksSequence1, PFpeaksSequence2, 'rows','complete');
+        % title([num2str(ithEnv1), num2str(ithEnv2), 'p=', num2str(PVAL)])
+        title(['r=', num2str(RHO)])
+        % colorbar; caxis([0, caxmax])
+        %title(['Env ID ', num2str(ithEnv)]); 
+        %xlabel('Position (2 cm bin)'); ylabel('Cell (sorted)');
+
+        %if ithEnv2==1; ylabel(['Env ', num2str(ithEnv2), ' data']); end
+
+    end
+end
+
 end
